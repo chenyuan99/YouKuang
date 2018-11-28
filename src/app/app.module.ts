e709f24eede2ddf522586feb0486fe313aff0155
WallfacerRZD <1048058400@qq.com>
2018-11-28 21:24:03 +0800

    布局

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {NgZorroAntdModule, NZ_I18N, zh_CN} from 'ng-zorro-antd';
+import {registerLocaleData} from '@angular/common';
+import zh from '@angular/common/locales/zh';
+import {HeaderComponent} from './main/header/header.component';
+import {FooterComponent} from './main/footer/footer.component';
+import {SiderComponent} from './main/sider/sider.component';
+import {ContentComponent} from './main/content/content.component';
+import {AppComponent} from './app.component';
+
+registerLocaleData(zh);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SiderComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContentComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    NgZorroAntdModule
   ],
-  providers: [],
+  providers: [{provide: NZ_I18N, useValue: zh_CN}],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/main/sider/sider.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-sider',
+  templateUrl: './sider.component.html',
+  styleUrls: ['./sider.component.css']
+})
+export class SiderComponent implements OnInit {
+  @Input()
+  isCollapsed = false;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
